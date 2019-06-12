75939b4d02f163915a3dac26e79f259584597191
choi <ww1016y@gmail.com>
2019-06-12 07:14:53 +0000

    first commit

---
+++ marvelmind_tf.cpp
@@ -0,0 +1,67 @@
+#include "ros/ros.h"
+#include "marvelmind_nav/hedge_pos_ang.h"
+#include "tf/transform_broadcaster.h"
+#include <nav_msgs/Odometry.h>
+
+class hedge_msg_adapter_node
+{
+public:
+  hedge_msg_adapter_node() 
+  {
+    ros::NodeHandle nh_; 
+    
+    // Init subscribers
+    pos_ang_sub_ = nh_.subscribe("hedge_pos_ang", 10, &hedge_msg_adapter_node::pos_ang_callback, this);
+
+    // Init publishers
+    hedge_pose_pub_ = nh_.advertise<nav_msgs::Odometry>("gps", 50);   
+ 
+    pose_out_.header.frame_id = "gps";
+    pose_out_.child_frame_id = "base_footprint";
+  }
+
+  void pos_ang_callback(const marvelmind_nav::hedge_pos_ang::ConstPtr& pos_ang_msg)
+  {
+
+    pose_out_.header.stamp = ros::Time::now();
+
+    pose_out_.pose.pose.position.x = pos_ang_msg->x_m;
+    pose_out_.pose.pose.position.y = pos_ang_msg->y_m;
+    pose_out_.pose.pose.position.z = pos_ang_msg->z_m;
+
+    pose_out_.pose.covariance[0] = 1;
+    pose_out_.pose.covariance[7] = 1;
+    pose_out_.pose.covariance[14] = 1;
+    pose_out_.pose.covariance[21] = 1;
+    pose_out_.pose.covariance[28] = 1;
+    pose_out_.pose.covariance[35] = 1;
+
+    pose_out_.pose.pose.orientation = tf::createQuaternionMsgFromYaw(pos_ang_msg->angle);
+	
+    ROS_INFO("X=%.3f  Y= %.3f  Z=%.3f  ", 	
+				//(int) hedge_pos_msg.address, 
+				//(int) hedge_pos_msg.timestamp_ms, 
+				(float) pose_out_.pose.pose.position.x, (float) pose_out_.pose.pose.position.y, (float) pose_out_.pose.pose.position.z);
+
+    hedge_pose_pub_.publish(pose_out_);
+  }
+
+
+protected:
+
+  ros::Subscriber pos_ang_sub_;
+  ros::Publisher hedge_pose_pub_;
+
+  // Message objects
+  nav_msgs::Odometry pose_out_;
+};
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "hedge_msg_adapter");
+
+  hedge_msg_adapter_node adapter;
+
+  ros::spin();
+}
+
